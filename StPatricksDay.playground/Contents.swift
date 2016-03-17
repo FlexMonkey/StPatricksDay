9d4afcb20e1d6dab5fcffea483166fc77e51315b
Simon Gladman <sgladman@gmail.com>
2016-03-17 09:55:43 +0000

    Initial commit

---
+++ StPatricksDay.playground/Contents.swift
@@ -0,0 +1,32 @@
+import UIKit
+import XCPlayground
+
+//: # Happy Saint Patricks's Day from [http://flexmonkey.blogspot.co.uk/](http://flexmonkey.blogspot.co.uk/)
+
+let layer = CAShapeLayer()
+layer.strokeColor = UIColor.greenColor().CGColor
+layer.fillColor = UIColor(red: 0.33, green: 0.5, blue: 0.33, alpha: 1).CGColor
+layer.lineWidth = 12
+layer.lineCap = kCALineCapRound
+layer.lineJoin = kCALineCapRound
+
+let view = UIView(frame: CGRect(x: 0, y: 0, width: 640, height: 640 ))
+view.layer.addSublayer(layer)
+
+XCPlaygroundPage.currentPage.liveView = view
+
+let points: [CGPoint] = 0.stride(to: M_PI * 4, by: 0.01).map
+{
+    let radius = (sin(2 * $0) + 0.25 * sin(6.0 * $0)) * 300
+    
+    let x = 320.0 + (cos($0) * radius)
+    let y = 320.0 + (sin($0) * radius)
+    
+    return CGPoint(x: x, y: y)
+}
+
+let path = CGPathCreateMutable()
+
+CGPathAddLines(path, nil, points, points.count)
+
+layer.path = path
